f0480d3118eb57e4fe85daebd08a08429ac240e7
malam <malam@axway.com>
2020-05-12 18:24:34 +0530

    additions & corrections

--- src/main/java/com/project/comicbook/dto/ProfileDto.java
+++ src/main/java/com/project/comicbook/dto/ProfileDto.java
@@ -1,11 +1,11 @@
 package com.project.comicbook.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class ProfileDto {
-
-    private Long id;
 
     private String name;
 
