327e378faf1aa697fda7e5b158a8421bc7c8671d
Md Zafar Alam <zafaralam2608@gmail.com>
2024-09-20 23:41:28 +0530

    Java 23 upgrade

--- src/main/java/com/project/comicbook/model/ProfileModel.java
+++ src/main/java/com/project/comicbook/model/ProfileModel.java
@@ -4,11 +4,11 @@ import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Date;
 
 @Entity
